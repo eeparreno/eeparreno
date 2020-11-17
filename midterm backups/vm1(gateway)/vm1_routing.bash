0c20e43c3c009c82aa3bc93fcacc2bee7dab286e
eeparreno <71423720+eeparreno@users.noreply.github.com>
2020-11-17 18:59:49 -0500

    Add files via upload

---
+++ midterm backups/vm1(gateway)/vm1_routing.bash
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+echo "Gateway SPR500 LAN segments"
+echo "192.168.1.0/24 via 172.16.1.1 dev ens192" >> /etc/sysconfig/network-scripts/route-ens192
+echo "192.168.2.0/24 via 172.16.2.1 dev ens192" >> /etc/sysconfig/network-scripts/route-ens192
+echo "192.168.3.0/24 via 172.16.3.1 dev ens192" >> /etc/sysconfig/network-scripts/route-ens192
+echo "192.168.4.0/24 via 172.16.4.1 dev ens192" >> /etc/sysconfig/network-scripts/route-ens192
+echo "192.168.5.0/24 via 172.16.5.1 dev ens192" >> /etc/sysconfig/network-scripts/route-ens192
+echo "192.168.6.0/24 via 172.16.6.1 dev ens192" >> /etc/sysconfig/network-scripts/route-ens192
+echo "192.168.7.0/24 via 172.16.7.1 dev ens192" >> /etc/sysconfig/network-scripts/route-ens192
+echo "192.168.8.0/24 via 172.16.8.1 dev ens192" >> /etc/sysconfig/network-scripts/route-ens192
+echo "192.168.9.0/24 via 172.16.9.1 dev ens192" >> /etc/sysconfig/network-scripts/route-ens192
+echo "192.168.10.0/24 via 172.16.10.1 dev ens192" >> /etc/sysconfig/network-scripts/route-ens192
+echo "192.168.11.0/24 via 172.16.11.1 dev ens192" >> /etc/sysconfig/network-scripts/route-ens192
+echo "192.168.12.0/24 via 172.16.12.1 dev ens192" >> /etc/sysconfig/network-scripts/route-ens192
+echo "192.168.13.0/24 via 172.16.13.1 dev ens192" >> /etc/sysconfig/network-scripts/route-ens192
+echo "192.168.14.0/24 via 172.16.14.1 dev ens192" >> /etc/sysconfig/network-scripts/route-ens192
+echo "192.168.15.0/24 via 172.16.15.1 dev ens192" >> /etc/sysconfig/network-scripts/route-ens192
+echo "192.168.16.0/24 via 172.16.16.1 dev ens192" >> /etc/sysconfig/network-scripts/route-ens192
+echo "192.168.17.0/24 via 172.16.17.1 dev ens192" >> /etc/sysconfig/network-scripts/route-ens192
+echo "192.168.18.0/24 via 172.16.18.1 dev ens192" >> /etc/sysconfig/network-scripts/route-ens192
+echo "192.168.19.0/24 via 172.16.19.1 dev ens192" >> /etc/sysconfig/network-scripts/route-ens192
+echo "192.168.20.0/24 via 172.16.20.1 dev ens192" >> /etc/sysconfig/network-scripts/route-ens192
+echo "192.168.21.0/24 via 172.16.21.1 dev ens192" >> /etc/sysconfig/network-scripts/route-ens192
+echo "192.168.22.0/24 via 172.16.22.1 dev ens192" >> /etc/sysconfig/network-scripts/route-ens192
+echo "192.168.23.0/24 via 172.16.23.1 dev ens192" >> /etc/sysconfig/network-scripts/route-ens192
+echo "192.168.25.0/24 via 172.16.25.1 dev ens192" >> /etc/sysconfig/network-scripts/route-ens192
+echo "192.168.26.0/24 via 172.16.26.1 dev ens192" >> /etc/sysconfig/network-scripts/route-ens192
+echo "192.168.27.0/24 via 172.16.27.1 dev ens192" >> /etc/sysconfig/network-scripts/route-ens192
+echo "192.168.28.0/24 via 172.16.28.1 dev ens192" >> /etc/sysconfig/network-scripts/route-ens192
+echo "192.168.29.0/24 via 172.16.29.1 dev ens192" >> /etc/sysconfig/network-scripts/route-ens192
+echo "192.168.30.0/24 via 172.16.30.1 dev ens192" >> /etc/sysconfig/network-scripts/route-ens192
